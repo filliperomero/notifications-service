5ddb6e912f9bf993fbf6a3689eb6bb88788730af
Fillipe Romero <fillipe.romero@gmail.com>
2022-12-18 16:14:04 -0700

    refactor code and add some tests

---
+++ src/application/entities/notification/notification.ts
@@ -0,0 +1,64 @@
+import { Replace } from 'src/helpers/Replace'
+import { Content } from './content'
+
+export interface NotificationProps {
+  recipientId: string
+  content: Content
+  category: string
+  readAt?: Date | null
+  createdAt: Date
+  updatedAt: Date
+}
+
+export class Notification {
+  private props: NotificationProps
+
+  constructor(props: Replace<NotificationProps, { createdAt?: Date; updatedAt?: Date }>) {
+    const newDate = new Date()
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? newDate,
+      updatedAt: props.updatedAt ?? newDate,
+    }
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId
+  }
+
+  public set content(content: Content) {
+    this.props.content = content
+  }
+
+  public get content(): Content {
+    return this.props.content
+  }
+
+  public set category(category: string) {
+    this.props.category = category
+  }
+
+  public get category(): string {
+    return this.props.category
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt
+  }
+
+  public get updatedAt(): Date {
+    return this.props.updatedAt
+  }
+}
